2acc05e08a45c373cd1fecd2ade6bb5c50f85442
이재원 <jenyglee_30@naver.com>
2024-05-14 21:10:38 +0900

    컬러 토큰 만들기

--- packages/themes/scripts/build-css-module.js
+++ packages/themes/scripts/build-css-module.js
@@ -3,12 +3,50 @@ import fs from 'fs';
 
 // console.log(Object.entries(theme.vars.colors.$static.light));
 
-Object.entries(theme.vars).forEach(([key, value]) => {
-  console.log(key, value.$static.light);
-  // Object.entries(value.$static.light).forEach(([key2, value2]) => {
-  //   console.log(key2, value2);
-  // });
-  //
-});
+// :root {
+//   --gray-900 : #1a202c;
+// }
 
-fs.writeFileSync('dist/theme.css', '');
+const toCssCasting = (str) => {
+  return str
+    .replace(/([a-z])(\d)/, '$1-$2')
+    .replace(/([A-Z])/g, '-$1')
+    .toLowerCase();
+};
+
+const generateThemeCssVariable = () => {
+  let cssValue = '';
+  Object.entries(theme.vars).forEach(([key, value]) => {
+    if (key === 'colors') {
+      Object.entries(value.$static).forEach(([colorKey, colorValue]) => {
+        if (colorKey === 'light') {
+          const selector = ':root';
+
+          const cssVariable = Object.entries(colorValue)
+            .map(([mainKey, mainValue]) =>
+              Object.entries(mainValue)
+                .map(
+                  ([subKey, subValue]) =>
+                    `--${toCssCasting(mainKey)}-${toCssCasting(
+                      subKey
+                    )} : ${subValue}`
+                )
+                .join('\n')
+            )
+            .join('\n');
+          // console.log('cssVariable : ', typeof cssVariable);
+          cssValue = `${selector} {\n${cssVariable}\n}`;
+        }
+      });
+    }
+  });
+  return cssValue;
+};
+
+const generateThemeCss = () => {
+  const variables = generateThemeCssVariable();
+  console.log('variables : ', variables);
+  fs.writeFileSync('dist/themes.css', variables);
+};
+
+generateThemeCss();
